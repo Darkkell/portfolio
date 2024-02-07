28cf9177cda780562cf04a7520c95f96c26686ba
Fermín Alejandro Flores Reyes <alejandro.flores.reyes@outlook.com>
2024-02-06 22:50:46 -0600

    Light, Dark and system mode

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	// darkMode: 'class', // if don't use a swith to dark mode delete this line
+	darkMode: 'class', // if don't use a swith to dark mode delete this line
 	theme: {
 		extend: {},
 	},
