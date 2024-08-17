5fe0003029a1676d48332dbec7a3719936891cf3
Fermín Alejandro Flores Reyes <alejandro.flores.reyes@outlook.com>
2024-08-17 16:19:23 -0600

    [UPDATE] icons, Projects.astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://www.dev1992.com/',
+  base: '/portfolio',
   integrations: [tailwind()]
 });
